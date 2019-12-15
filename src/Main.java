2c55ad6a0ea5042e99361271498f4104249f2be4
Rostislavn86 <Rostislavfromminsk@gmail.com>
2019-12-15 22:13:12 +0300

    Финальная версия, привести код в порядок, найти и справить помарки

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,39 @@ public class Main
     {
         Methods methods = new Methods();
 
-        Book createbook1= new Book(methods.CreateId());
+        //список книг
+
+//        Михаил Булгаков — Мастер и Маргарита
+//        Федор Достоевский — Преступление и наказание
+//        Лев Толстой — Война и мир
+//        Михаил Булгаков — Собачье сердце
+//        Федор Достоевский — Идиот
+//        Федор Достоевский — Братья Карамазов
+//        Илья Ильф, Евгений Петров — Двенадцать стульев
+//        Николай Гоголь — Мёртвые души
+//        Иван Тургенев — Отцы и дети
+//        Лев Толстой — Анна Каренина
+
+    ObjMassOfBooksAndMethods objMassOfBooksAndMethods = new ObjMassOfBooksAndMethods();
+
+
+        Book createbook1= new Book(objMassOfBooksAndMethods.CreateId());
         Book createBook2 = new Book(methods.CreateId(),"asda","asdasd","dasda",1992,10,199,"type");
-        Book createBook3 = new Book(122,"asda","asdasd","dasda",1992,10,199,"type");
+        Book createBook3 = new Book(methods.CreateId(),"Третья книга","asdasd","dasda",1992,10,199,"type");
         System.out.println(createbook1.getId());
         System.out.println(createBook2.getId());
 
         System.out.println(createbook1.getId());
 
-        System.out.println(createbook1.toString());
+        System.out.println(createBook3.toString());
 
         //    if (createbook1.getNameBok().equals("null")) System.out.println("0");
 
         System.out.println(Book.getNumberOfInstance());
+
+        System.out.println(objMassOfBooksAndMethods.library[0] = createBook3);
+        System.out.println(objMassOfBooksAndMethods.library[1] = createbook1);
+
 
     }
 }
--- src/ObjMassOfBooksAndMethods.java
+++ src/ObjMassOfBooksAndMethods.java
@@ -1,8 +1,14 @@
-public class ObjMassOfBooks
+public class ObjMassOfBooksAndMethods
 {
 
 
     Book[] library = new Book[10];
 
+    public int CreateId()
+    {
+        //Создадим для каждой книги свой уникальный id, пусть id будет пятизначным
+        //Book.getNumberOfInstance();
+        return 12345678;
+    }
 
 }
